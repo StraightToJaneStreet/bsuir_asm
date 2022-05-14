de87e1910ab9779eee1d799683469f3556ad7c37
Tatally Not a JSC Enjoyer <straighttojanestreet@rs.app>
2022-05-14 21:31:53 +0300

    feat: add ipr 1-15 solution

--- task_ipr-1-15.asm
+++ task_ipr-1-15.asm
@@ -15,17 +15,46 @@ section '.code' code readable executable
   
   invoke printf, msg_input
   invoke scanf, input_fmt, input_source
-
-  invoke printf, output_fmt, input_source
+  
+  mov ebx, 0
+  mov ecx, input_source
+  
+  main_loop:  
+    mov al, byte [ecx]
+    sub al, 'c'
+    je inc_counter
+    
+    mov al, byte [ecx]
+    sub al, 'd'
+    je inc_counter
+    
+    mov al, 0
+    or al, byte [ecx]
+    je end_loop
+    
+    continue:
+      inc ecx
+      jmp main_loop
+      
+    inc_counter: 
+      inc ebx
+      jmp continue
+      
+  end_loop:
+  
+  mov dword [result], ebx
+  
+  invoke printf, output_fmt, [result]
   invoke getch
   invoke exit, 0
 
 section '.data' data readable writeable
   msg_input db 'Source: ', 0
   input_fmt db '%s', 0
-  output_fmt db 'Result: %s', 0
   
-  input_source db 100 dup(2)
+  output_fmt db 'Result: %d', 0
+  result dd 0
+  input_source db 100 dup(0)
 
 
 
